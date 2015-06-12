2e9dfb04dddc049c96729bcd6942d2ad719913cc
Wai Ho Leung <whleung@live.com>
2015-06-11 21:21:38 -0700

    Added Python solution for problem 19.

---
+++ problem19.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+count = 0
+
+for year in xrange(1901, 2001):
+    for month in xrange(1, 13):
+        if date(year, month, 1).weekday() == 6:
+            count += 1
+
+print count
+
